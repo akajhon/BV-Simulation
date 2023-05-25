993c48f140d9629616e889eed9ca56d17d527d13
João Pedro <69048604+akajhon@users.noreply.github.com>
2023-05-25 00:45:39 -0300

    Add files via upload

--- Simulation/init.sh
+++ Simulation/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+handle_sigint() {
+    echo -e "\033[32m[!] CTRL+C pressionado! Parando todos os serviços... [!]\033[0m"
+    sudo docker-compose down
+    exit
+}
+
+trap "handle_sigint" 2
+
 cat << "EOF"
 ****************************************************************************
    ___  _   __      ____ ____ __  ___ __  __ __    ___  ______ ____   ___ 
@@ -10,24 +18,24 @@ cat << "EOF"
 ****************************************************************************
 EOF
 
-echo "\033[32m[!] Construindo e executando containers Docker... [!]\033[0m"
+echo -e "\033[32m[!] Construindo e executando containers Docker... [!]\033[0m"
 
 sudo docker-compose build
 
 if [ $? -ne 0 ]; then
-  echo "\033[31m[!] A construção dos serviços falhou... [!]\033[0m"
+  echo -e "\033[31m[!] A construção dos serviços falhou... [!]\033[0m"
   exit 1
 fi
 
 sudo docker-compose up -d
 
 if [ $? -ne 0 ]; then
-  echo "\033[31m[!] A execução dos serviços falhou... [!]\033[0m"
+  echo -e "\033[31m[!] A execução dos serviços falhou... [!]\033[0m"
   exit 1
 fi
 
-echo "\033[32m[!] Todos os serviços foram construídos e estão em execução! [!]\033[0m"
+echo -e "\033[32m[!] Todos os serviços foram construídos e estão em execução! [!]\033[0m"
 
-echo "\033[32m[!] Logs dos serviços em Execução: [!]\033[0m"
+echo -e "\033[32m[!] Logs dos serviços em Execução: [!]\033[0m"
 
 sudo docker-compose logs --tail=0 --follow
